b4fb29cb1d6b60476d1b922442e0b8a0d066a81a
Huaqing Xu <huaqingxu89@gmail.com>
2022-09-24 13:18:00 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # taichi
 Taichi usage example.
+
+## primes count
+
+```sh
+python count_primes_without_taichi.py
+
+50000000
+time cost: 45.261165142059326s
+# =============================
+
+python count_primes_with_taichi.py
+
+[Taichi] version 1.1.3, llvm 10.0.0, commit 1262a70a, osx, python 3.9.12
+[Taichi] Starting on arch=arm64
+50000000
+time cost: 0.09768223762512207s
+```
+
---
+++ count_primes_with_taichi.py
@@ -0,0 +1,26 @@
+"""Count the number of primes in range [1, n].
+"""
+import time
+import taichi as ti
+ti.init()
+
+@ti.func
+def is_prime(n: int):
+    result = True
+    for k in range(2, int(n**0.5)+1):
+        if n % k == 0: result = False
+        break
+    return result
+
+@ti.kernel
+def count_primes(n: int) -> int:
+    count = 0
+    for k in range(2, n):
+        if is_prime(k): count += 1
+    return count
+
+start_time = time.time()
+print(count_primes(100000000))
+end_time = time.time()
+print("time cost: {}s".format(end_time-start_time))
+
