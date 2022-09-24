4baa8804c0d5ec798ac9e075124d60440b9a6497
Huaqing Xu <huaqingxu89@gmail.com>
2022-09-24 13:27:02 +0800

    update

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Taichi usage example.
 ```sh
 python count_primes_without_taichi.py
 
-50000000
-time cost: 45.261165142059326s
+78498
+time cost: 3.318950891494751s
 # =============================
 
 python count_primes_with_taichi.py
 
 [Taichi] version 1.1.3, llvm 10.0.0, commit 1262a70a, osx, python 3.9.12
 [Taichi] Starting on arch=arm64
-50000000
-time cost: 0.09768223762512207s
+78498
+time cost: 0.08886313438415527s
 ```
 
--- count_primes_with_taichi.py
+++ count_primes_with_taichi.py
@@ -8,19 +8,21 @@ ti.init()
 def is_prime(n: int):
     result = True
     for k in range(2, int(n**0.5)+1):
-        if n % k == 0: result = False
-        break
+        if n % k == 0: 
+            result = False
+            break
     return result
 
 @ti.kernel
 def count_primes(n: int) -> int:
     count = 0
     for k in range(2, n):
-        if is_prime(k): count += 1
+        if is_prime(k): 
+            count += 1
     return count
 
 start_time = time.time()
-print(count_primes(100000000))
+print(count_primes(1000000))
 end_time = time.time()
 print("time cost: {}s".format(end_time-start_time))
 
